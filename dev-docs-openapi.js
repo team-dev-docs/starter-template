734dc76ad1241a825f0222d32c07f7c8ee701b0b
andrew.ryan.vanbeek@gmail.com <andrewvanbeek@andrews-MacBook-Air-2.local>
2023-12-15 11:13:54 -0800

    before openapi work

--- dev-docs-openapi.js
+++ dev-docs-openapi.js
@@ -1,5 +1,11 @@
 const apiConfig = {
-
+   petstore: { // the <id> referenced when running CLI commands
+      specPath: "examples/petstore.yaml", // path to OpenAPI spec, URLs supported
+      outputDir: "api/petstore", // output directory for generated files
+      sidebarOptions: { // optional, instructs plugin to generate sidebar.js
+        groupPathsBy: "tag", // group sidebar items by operation "tag"
+      }
+    }
 }
 
 module.exports = {
