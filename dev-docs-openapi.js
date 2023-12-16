363c1fd801632b207d14b948f865f2f12836e6ea
andrew.ryan.vanbeek@gmail.com <andrewvanbeek@andrews-MacBook-Air-2.local>
2023-12-16 08:17:54 -0800

    this is good

--- dev-docs-openapi.js
+++ dev-docs-openapi.js
@@ -1,13 +1,176 @@
-const apiConfig = {
-   petstore: { // the <id> referenced when running CLI commands
-      specPath: "examples/petstore.yaml", // path to OpenAPI spec, URLs supported
-      outputDir: "api/petstore", // output directory for generated files
-      sidebarOptions: { // optional, instructs plugin to generate sidebar.js
-        groupPathsBy: "tag", // group sidebar items by operation "tag"
+var Converter = require("openapi-to-postmanv2");
+const fs = require("fs");
+function parseOpenApiInfo(openapiData) {
+   const operation = openapiData.api;
+   const headers = {};
+   const body = {};
+   const query = {};
+ 
+   // Parse security schemes
+   if (operation.securitySchemes) {
+     for (const [schemeName, schemeData] of Object.entries(operation.securitySchemes)) {
+       switch (schemeData.type) {
+         case "apiKey":
+           if (schemeData.in === "header") {
+             headers[schemeData.name] = "[API Key]"; // Placeholder for actual key
+           }
+           break;
+         case "oauth2":
+           // Handle OAuth2 details based on flow type
+           break;
+         default:
+           // Handle other security scheme types
+           break;
+       }
+     }
+   }
+ 
+   // Parse request body schema
+   if (operation.requestBody) {
+     const content = operation.requestBody.content["application/json"];
+     if (content && content.schema) {
+       const schema = content.schema;
+       parseSchema(schema, "", body);
+     }
+   }
+ 
+   // Parse path parameters (simulated as query for simplicity)
+   const pathParams = operation.path.match(/\{[^}]+\}/g);
+   if (pathParams) {
+     for (const param of pathParams) {
+       const paramName = param.slice(1, -1);
+       query[paramName] = "[Path Parameter: " + paramName + "]"; // Placeholder
+     }
+   }
+ 
+   // Parse query parameters (if any)
+   // TODO: Implement logic to parse actual query parameters from request
+ 
+   return { headers, body, query };
+ }
+ 
+ function parseSchema(schema, parentPath, targetObj) {
+   if (schema.type === "object") {
+     for (const [key, value] of Object.entries(schema.properties)) {
+       const fullPath = parentPath ? `${parentPath}.${key}` : key;
+       parseSchema(value, fullPath, targetObj);
+     }
+   } else if (schema.type === "array") {
+     // Handle arrays (simple example, can be expanded)
+     targetObj[parentPath] = "[Array]";
+   } else {
+     // Handle basic types (string, number, etc.)
+     targetObj[parentPath] = "[Placeholder]";
+   }
+ }
+
+
+function findMatchingItem(items, metadata) {
+    // Check if items is not an array or is empty
+    if (!Array.isArray(items) || items.length === 0) {
+        return null;
+    }
+
+    for (const item of items) {
+        // Check if the item has a request and matches the metadata
+        if (item?.request?.url?.path && metadata?.api?.path) {
+            const pathMatches = `/${item.request.url.path.join("/")}` === metadata.api.path;
+            const methodMatches = item.request.method.toLowerCase() === metadata.api.method.toLowerCase();
+
+            if (pathMatches && methodMatches) {
+                return item;
+            }
+        }
+
+        // If the current item has nested items, search recursively
+        if (item.item) {
+            console.log("item.item", item.item)
+            const foundItem = findMatchingItem(item.item, metadata);
+            if (foundItem) {
+                return foundItem;
+            }
+        }
+    }
+
+    let justUrls = items.map(item => {
+      if(item?.request?.url?.path) {
+          return `/${item.request.url.path.join("/")}`
+      } else {
+          return null
       }
+   })
+
+   console.log("this is justUrls", justUrls)
+   console.log("this is metadata",  metadata.api.path)   
+
+    // Return null if no matching item is found
+    return null;
+}
+
+function generatePostmanItem(item) {
+   try {
+      const yamlData = fs.readFileSync(`examples/${petstore}.yaml`, "utf8");
+
+      let conversionResult;
+      let errorOccurred = false;
+
+      Converter.convert(
+        { type: "string", data: yamlData },
+        {},
+        (err, result) => {
+          if (err) {
+            console.error(err);
+            errorOccurred = true;
+          } else {
+            conversionResult = result;
+          }
+        }
+      );
+
+      if (errorOccurred || !conversionResult) {
+        return ""; // Return an empty string in case of error
+      }
+      let apiItems = conversionResult.output[0].data.item
+      let flatItems = []
+      for(let apiItem of apiItems) {
+        flatItems = [...flatItems, ...apiItem.item]
+      }
+
+        let finalItem = findMatchingItem(flatItems, metadata);
+        console.log("finalItem", finalItem != undefined)
+        console.log("finalItem", finalItem)
+        metadata.postmanItem = finalItem
+     //  if(metadata.postmanItem) fs.writeFileSync("output.json", JSON.stringify(metadata, null, 2))
+     //  return JSON.stringify(metadata, null, 2);
+    } catch (err) {
+      console.error(err);
+      return ""; // Return an empty string in case of error
     }
 }
 
-module.exports = {
-   config: apiConfig
+function generateTemplate(metadata) {
+   
 }
+
+
+const apiConfig = {
+  petstore: {
+    // the <id> referenced when running CLI commands
+    specPath: "examples/pestore.yaml", // path to OpenAPI spec, URLs supported
+    outputDir: "docs/api/petstore", // output directory for generated files
+    sidebarOptions: {
+      // optional, instructs plugin to generate sidebar.js
+      groupPathsBy: "tag", // group sidebar items by operation "tag"
+    },
+    markdownGenerators: {
+      createApiPageMD: (metadata) => {
+         // console.log("metadata", metadata);
+         
+      },
+    },
+  },
+};
+
+module.exports = {
+  config: apiConfig,
+};
--- src/components/JsonToTable.jsx
+++ src/components/JsonToTable.jsx
@@ -2,25 +2,25 @@ import React, { useState, useEffect } from 'react';
 import zlib from 'zlib';
 import { useDoc } from '@docusaurus/theme-common/internal';
 
-const JsonToTable = () => {
+const JsonToTable = ({ data }) => {
   const [decodedData, setDecodedData] = useState({});
 
   const { frontMatter } = useDoc();
 
   useEffect(() => {
-    if (frontMatter.api) {
+    if (data) {
+      console.log('data', data);
       try {
         let decodedJSON = JSON.parse(
-          zlib.inflateSync(Buffer.from(frontMatter.api, 'base64')).toString()
+         data
         );
         console.log('decodedJSON', decodedJSON);
-        decodedJSON.requestBodyValues = extractPropertiesAndExamples(decodedJSON);
         setDecodedData(decodedJSON);
       } catch (error) {
         console.error('Error parsing JSON:', error);
       }
     }
-  }, [frontMatter.api]);
+  }, [data]);
 
   const extractPropertiesAndExamples = (json) => {
     try {
@@ -68,64 +68,30 @@ const JsonToTable = () => {
   };
 
 
-  return (
-    <div>
-      <div>
-        {decodedData.info && (
-          <>
-            <h2>{decodedData.info.title}</h2>
-            <p>{decodedData.info.description}</p>
-          </>
-        )}
-        {decodedData.parameters && decodedData.parameters.length > 0 && (
-          <>
-            <h3>Parameters:</h3>
-            <ul>
-              {decodedData.parameters.map((param) => (
-                <li key={param.name}>
-                  <strong>{param.name}:</strong> {param.description}
-                </li>
-              ))}
-            </ul>
-          </>
-        )}
-        {decodedData?.requestBodyValues && Object.keys(decodedData.requestBodyValues).length > 0 && (
-          <>
-            <h3>Request Body</h3>
+  const renderTable = () => {
+    if (!data.body) return null;
 
-              {Object.entries(decodedData.requestBodyValues).map(([key, value]) => (
-                
-                <div style={{ borderTop: '1px solid #eaecef', margin: '24px 0' }} aria-hidden="true" key={key}>
-                  <strong>{key}:</strong> 
-                  <p>{value.description}</p>
-                  <br></br>
-                  Extra Context {JSON.stringify(value)}
-                </div>
-              ))}
-           
-          </>
-        )}
-        {decodedData.securitySchemes && Object.keys(decodedData.securitySchemes).length > 0 && (
-          <>
-            <h3>Security Schemes:</h3>
-            <ul>
-              {Object.entries(decodedData.securitySchemes).map(([key, value]) => (
-                <li key={key}>
-                  <strong>{key}:</strong> {value.description}
-                </li>
-              ))}
-            </ul>
-          </>
-        )}
-        {decodedData.postman && decodedData.postman.description && (
-          <>
-            <h3>Postman Configuration:</h3>
-            <p>{decodedData.postman.description.content}</p>
-          </>
-        )}
-      </div>
-    </div>
-  );
+    const tableRows = Object.entries(data.body).map(([key, value]) => (
+      <tr key={key}>
+        <td>{key}</td>
+        <td>{value}</td>
+      </tr>
+    ));
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Key</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>{tableRows}</tbody>
+      </table>
+    );
+  };
+
+  return <div>{renderTable()}</div>;
 };
 
 export default JsonToTable;
